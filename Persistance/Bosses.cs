0afe6b78492f16e08c3775e4e55a49c9fbad9ed8
chepeguicho <joseluisdmgua@yahoo.com>
2024-10-11 23:29:51 -0600

    Fixes error when naming new strikes

--- Persistance/Bosses.cs
+++ Persistance/Bosses.cs
@@ -187,7 +187,7 @@ namespace falcon.cmtracker.Persistance
 
             var Cosmic_Observatory = new Token();
             Cosmic_Observatory.Id = 77302;
-            Cosmic_Observatory = "Cosmic Observatory";
+            Cosmic_Observatory.Name = "Cosmic Observatory";
             Cosmic_Observatory.Icon = "icon_cosmic_observatory.png";
             Cosmic_Observatory.setting = SettingUtil.GetSettingForBoss(setting, Module.CURRENT_ACCOUNT.Value, Boss.Cosmic_Observatory);
             Cosmic_Observatory.bossType = BossType.Strike;
@@ -195,13 +195,11 @@ namespace falcon.cmtracker.Persistance
 
             var Temple_Of_Febe = new Token();
             Temple_Of_Febe.Id = 77302;
-            Temple_Of_Febe = "Temple of Febe";
+            Temple_Of_Febe.Name = "Temple of Febe";
             Temple_Of_Febe.Icon = "icon_temple_of_febe.png";
             Temple_Of_Febe.setting = SettingUtil.GetSettingForBoss(setting, Module.CURRENT_ACCOUNT.Value, Boss.Temple_Of_Febe);
             Temple_Of_Febe.bossType = BossType.Strike;
             tokens.Add(Temple_Of_Febe);
-
-
 
             return tokens;
         }
